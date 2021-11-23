507cb01a4721d5429ab3fe96f1d797fc1739522f
Ishaque Ahmed <ishaqueahmednasim@gmail.com>
2021-11-23 20:29:05 +0600

    project deployment one

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors'); //Cross origin resource sharing
+const compression = require('compression');
+const userRouter = require('./routers/userRouter');
+const orderRouter = require('./routers/orderRouter');
+
+const app = express();
+
+app.use(compression());
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/user', userRouter);
+
+app.use('/order', orderRouter);
+
+
+module.exports = app;
